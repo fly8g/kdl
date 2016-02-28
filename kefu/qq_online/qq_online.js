574048efb3e0d7545b2e1aec1cbfd46bbb9e5642
fly8g <tangling-2004@163.com>
2016-02-28 13:26:44 +0800

    v1

---
+++ kefu/qq_online/qq_online.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$("#scrollsidebar").fix({
+		float : 'right',
+		//minStatue : true,
+		skin : 'green',
+		durationTime : 600
+	});
+
+	$("#scrollsidebar2").fix({
+		float : 'right',
+		minStatue : true,
+		skin : 'gray',
+		durationTime : 1000
+	});
+
+});
