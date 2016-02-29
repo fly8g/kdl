a1c17dbfd1a539b8318d8772232cc3bdefc7b801
fly8g <tangling-2004@163.com>
2016-03-01 02:16:46 +0800

    头部下拉菜单作成

--- data/common.inc.php
+++ data/common.inc.php
@@ -4,6 +4,17 @@ $cfg_dbhost = 'localhost';
 $cfg_dbname = 'dede_html5jianzhu';
 $cfg_dbuser = 'root';
 $cfg_dbpwd = 'fly8g@2015';
+
+//$cfg_dbhost = '192.168.0.104';
+//$cfg_dbname = 'dede_html5jianzhu';
+//$cfg_dbuser = 'fly8g';
+//$cfg_dbpwd = 'tl100419';
+
+//$cfg_dbhost = '112.74.82.156';
+//$cfg_dbname = 'sxkdl';
+//$cfg_dbuser = 'sxkdl';
+//$cfg_dbpwd = 'R8F5f3W2';
+
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
 
